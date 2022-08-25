96814ebd0bd63685ca3d87ebffe52adce48e6d46
sanat jain <nj796644@gmail.com>
2022-08-25 16:11:27 +0530

    login php

---
+++ cSignup.php
@@ -0,0 +1,29 @@
+<?php
+
+$lgntype = $_GET['lgntype'];
+$email = $_GET['email'];
+$pass = $_GET['pass'];
+$con = mysqli_connect('localhost', 'root');
+mysqli_select_db($con,'apex-business-match');
+
+if(!$con)
+echo "connection failed"; 
+else {
+    if($lgntype=="investor"){
+        $q = "insert into investerlogin (Email,Password) values ('$email','$pass')";
+        $error = mysqli_query($con,$q);
+    }
+    else if($lgntype=="ent")
+    {
+        $q = "insert into entlogin (Email,Password) values ('$email','$pass')";
+        $error = mysqli_query($con,$q);
+    }
+}
+
+
+?>
+
+
+
+
+
