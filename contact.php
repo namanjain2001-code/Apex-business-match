026cff17fb2675542033ac03f3344ece5dcaa4f9
Swayamsanghvi <89508673+Swayamsanghvi@users.noreply.github.com>
2022-10-26 20:18:16 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,1 +1,15 @@
 
+<html>
+  <body>
+    <h1>contact details</h1>
+<form action="/page.php">
+  <label for="email">EMAIL:</label>
+  <input type="text" id="email" name="email"><br><br>
+  <label for="pno.">PHONE NUMBER:</label>
+  <input type="text" id="pno." name="pno."><br><br>
+  <input type="submit" value="Submit">
+</form>
+    <p>Click the "Submit" button and the form-data will be sent to a page on the 
+server called "page.php".</p>
+</body>
+</html>
